eb2f5345b1b14fb7a105cb2b073f190d09a2596a
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2025-03-08 16:35:49 -0500

    Configure lambda functions in handler, add requirements txt

--- serverless-dir/handler.py
+++ serverless-dir/handler.py
@@ -1,10 +1,31 @@
 import json
 
 
-def hello(event, context):
+def function_1(event, context):
+    body ={
+        "First Name": "Sebastián",
+        "Surname": "Cruz",
+        "Age": 34,
+        "Favourite Colour": "Blue",
+        "Favourite Game": "Age of empires"
+    }
+
+    response = {"statusCode": 200, "body": json.dumps(body)}
+
+    return response
+
+def function_2(event, context):
     body = {
-        "message": "Go Serverless v3.0! Your function executed successfully!",
-        "input": event,
+        "One" : 1,
+        "Two" : 2,
+        "Three" : 3,
+        "Four" : 4,
+        "Five" : 5,
+        "Six" : 6,
+        "Seven" : 7,
+        "Eight" : 8,
+        "Nine" : 9,
+        "Ten" : 10,
     }
 
     response = {"statusCode": 200, "body": json.dumps(body)}
